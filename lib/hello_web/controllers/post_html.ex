950f60dddc64124d733842b392a1d588438a9e8a
wernerlaude <mail@laude.dev>
2025-02-22 20:23:22 +0100

    Posts added

---
+++ lib/hello_web/controllers/post_html.ex
@@ -0,0 +1,13 @@
+defmodule HelloWeb.PostHTML do
+  use HelloWeb, :html
+
+  embed_templates "post_html/*"
+
+  @doc """
+  Renders a post form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def post_form(assigns)
+end
